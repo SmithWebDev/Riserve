2d35756d02fdf18210bca5445199ea8b43487e9d
Smith <smithwebdd@gmail.com>
2019-07-28 21:05:09 -0500

    home and about page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "home#index"
+  # get 'home/about'
+  get "/about", to: "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
