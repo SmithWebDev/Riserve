51978808cddfcf8fb99ce4c8c931fc580719cd6e
Smith <smithwebdd@gmail.com>
2019-07-30 15:34:04 -0500

    sq headache #1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @pkey = ENV['P_KEY']
     @api = StockQuote::Stock.new(api_key: '@pkey')
+    # @stock  = StockQuote::Stock.quote('fb')
+    # @stock = StockQuote::Stock.quote('params[:ticker]')
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  # get 'home/about'
-  get "/about", to: "home#about"
+  get  "/about", to: "home#about"
+  post "/",      to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
